92ab32003428a15b3175973a4fdd5e2c5cd98259
thiagom <thiago.gm.pereira@gmail.com>
2017-12-05 00:19:41 -0200

    Replaced different activities by Fragment manipulation

--- app/src/main/java/nanodegree/thiago/bakingapp_stage1/ui/MainActivity.java
+++ app/src/main/java/nanodegree/thiago/bakingapp_stage1/ui/MainActivity.java
@@ -2,34 +2,63 @@ package nanodegree.thiago.bakingapp_stage1.ui;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import nanodegree.thiago.bakingapp_stage1.R;
 import nanodegree.thiago.bakingapp_stage1.data.RecipeJson;
 
-public class MainActivity extends AppCompatActivity implements RecipesListFragment.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity
+        implements RecipesListFragment.OnFragmentInteractionListener,
+        StepsListFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         if (savedInstanceState == null) {
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            Log.d("MAIN", "" +transaction.isEmpty());
 
             RecipesListFragment fragment = new RecipesListFragment();
-            transaction.replace(R.id.recipes_list_fragment, fragment);
+            transaction.replace(R.id.fragments_container, fragment);
             transaction.commit();
         }
     }
 
     @Override
     public void onFragmentInteraction(RecipeJson recipe) {
-        // TODO: Here I'l need to check if single fragment screen, or tablet.. etc..
-        Intent intent = new Intent(this, RecipeStepsActivity.class);
-        intent.putExtra(getString(R.string.extra_recipe), recipe);
+        // TODO: Here I'll need to check if single fragment screen, or tablet.. etc..
+        //Intent intent = new Intent(this, RecipeStepsActivity.class);
+       // intent.putExtra(getString(R.string.extra_recipe), recipe);
 
-        startActivity(intent);
+        //startActivity(intent);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        StepsListFragment fragment = new StepsListFragment();
+        fragment.setStepsList(recipe.getSteps());
+
+        transaction.replace(R.id.fragments_container, fragment, "RECIPES_FRAGMENT");
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (getFragmentManager().getBackStackEntryCount() > 0) {
+            getFragmentManager().popBackStack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
